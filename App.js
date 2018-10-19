e65bbaf6b6319688c539c3963d980bebfcd0992b
Aaron Parsons <aaronparsons@Aarons-MacBook-Pro.local>
2018-10-19 16:10:05 -0600

    stated implementing map markers

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import { Header } from 'react-native-elements';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      region: {
+        latitude: 51.0478,
+        longitude: -114.0593,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      },
       latitude: 0,
       longitude: 0,
-      statusBarHeight: 1,
+      markers: [{
+        id: 1,
+        longitude: 51.0478,
+        latitude: -114.0593,
+        title: 'title',
+        description: "description"
+      }],
     }
+    this.socket = new WebSocket("ws://172.16.203.34:8000/");;
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  createMarkers() {
+    this.state.markers.map((marker) => {
+      console.log('marker console log', marker);
+      return (
+        <Marker
+          key={marker.id}
+          coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+          title={marker.title}
+          description={marker.description}
+        />
+      );
+    });
   }
 
   componentDidMount() {
+    this.socket.onopen = (event) => {
+    };
+
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      // this.setState({markers: data});
+    }
+
     navigator.geolocation.getCurrentPosition(function(e) {
-      console.log(e);
       this.setState({
         latitude: e.coords.latitude,
         longitude: e.coords.longitude
@@ -24,6 +63,8 @@ export default class App extends React.Component {
   }
 
   render() {
+    const markers = this.createMarkers();
+
     return (
       <View style={{ width: '100%', height: '100%' }}>
         <Header
@@ -33,14 +74,12 @@ export default class App extends React.Component {
         />
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 51.0478,
-            longitude: -114.0593,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1,
-          }}
+          region={this.state.region}
+          // onRegionChange = {this.onRegionChange}
           showsUserLocation={true}
-        />
+        >
+          {markers}
+        </MapView>
       </View>
     );
   }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const WebSocket = require('ws');
 
+const spotsArray = [
+  {
+    id: 1,
+    longitude: 51.0478,
+    latitude: -114.0593,
+    title: 'title',
+    description: "description"
+  },
+  {
+    id : 2,
+    longitude: 51.0278,
+    latitute: -114.0493,
+    title: 'title',
+    description: "description"
+  },
+  {
+    id: 3,
+    longitude: 51.0538,
+    latitute: -114.0123,
+    "title": 'title',
+    "description": "description"
+  },
+  {
+    id: 4,
+    longitude: 51.0522,
+    latitude: -114.0519,
+    title: 'title',
+    description: "description"
+  }
+];
+
 // Set the port to 3001
-const PORT = 3001;
+const PORT = 8000;
 
 // Create a new express server
 const server = express()
@@ -13,11 +44,26 @@ const server = express()
 // Create the WebSockets server
 const wss = new WebSocket.Server({ server });
 
+// Broadcast to all users function
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+function getSpots() {
+  wss.broadcast(JSON.stringify(spotsArray));
+}
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  // Get all markers
+  getSpots();
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
