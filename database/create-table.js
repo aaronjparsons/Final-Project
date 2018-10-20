b90e2fd6d83e8c63f33a57ab979afaf59e65d530
Thofeeq <thofeeqm@gmail.com>
2018-10-20 14:29:33 -0600

    Added seed file for user and contract

--- database/create-table.js
+++ database/create-table.js
@@ -6,7 +6,7 @@ var knex = require('knex')({
   client: 'postgresql',
   connection: {
     host     : process.env.DB_HOST,
-    user     : "process.env.DB_USER",
+    user     : process.env.DB_USER,
     password : process.env.DB_PASS,
     database : process.env.DB_NAME,
     port     : process.env.DB_PORT,
