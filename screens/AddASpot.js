b6f3a3da570390c3a0b094ba3f0081b4f65ed355
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-10-23 15:35:10 -0600

    added Add A Spot page

--- screens/AddASpot.js
+++ screens/AddASpot.js
@@ -1,16 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, StatusBar, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, Button, TextInput } from 'react-native';
 
 
 
 export default class AddASpot extends React.Component {
-
   render() {
-
     return (
-      <View>
-        <Text>Add a Spot Page</Text>
+      <View style={styles.body}>
+        <View style={styles.headerContent}>
+          <Text>Add a Parking Spot</Text>
+        </View>
+        <View style={styles.content}>
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(text) => this.setState({input: text})}
+            placeholder={'Address'}
+          />
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(text) => this.setState({input: text})}
+            placeholder={'Picture URL'}
+          />
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(text) => this.setState({input: text})}
+            placeholder={'Description'}
+          />
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(text) => this.setState({input: text})}
+            placeholder={'Price'}
+          />
+        </View>
+        <Button
+          style={styles.button}
+          onPress={(e) => console.log(e)}
+          title="Save Changes"
+          // color="blue"
+          accessibilityLabel="Change User Profile"
+        />
       </View>
-    );
+      
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  body:{
+    backgroundColor: "white",
+    height:800,
+    alignItems:'center',
+  },
+  inputField: {
+    height: 40, 
+    width: 250, 
+    borderColor: 'black', 
+    borderWidth: 1,
+    borderRadius: 10,
+    alignItems: 'center',
+    margin: 10,
+    padding: 5
+  },
+  button: {
+    width: 300,
+    color: 'blue',
+  },
+  headerContent:{
+    padding:30,
+    // fontSize: 10,
+    alignItems: 'center',
+  },
+  // content: {
+  //   flex:1,
+  //   alignItems:'flex-start',
+  //   paddingLeft:5
+  // }
+})
