417b3e61092c880e96450f253905c77e335600b1
Dave S <dave.samson91@gmail.com>
2018-10-31 18:51:22 -0600

    faq styled

--- screens/Help.js
+++ screens/Help.js
@@ -30,14 +30,14 @@ export default class Help extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.scrollview}>
-        <Container>
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollview}>
           <Header style={styles.header}>
             <Text
               onPress={() => this.props.navigation.navigate("Home")}
               style={styles.parker}
             >
-              Stable
+              STABLE
             </Text>
             <Button
               style={styles.button}
@@ -49,10 +49,10 @@ export default class Help extends React.Component {
           <Content>
             <Text style={styles.title}>Help /FAQ</Text>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: How long can I park here?</Text>
-                  <Text>
+                  <Text style={styles.body}>Q: How long can I park here?</Text>
+                  <Text style={styles.body}>
                     A: As long as you want! Don't forget to stop the meter when
                     you are done parking.
                   </Text>
@@ -60,10 +60,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: I'm experinceing technical issues with Stable?</Text>
-                  <Text onPress={this.handleEmail}>
+                  <Text style={styles.body}>
+                    Q: I'm experinceing technical issues with Stable?
+                  </Text>
+                  <Text style={styles.body} onPress={this.handleEmail}>
                     A: Please email our support team.
                   </Text>
                   <Text onPress={this.handleEmail} style={styles.email}>
@@ -73,10 +75,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: Is Stable only available in Calgary?</Text>
-                  <Text>
+                  <Text style={styles.body}>
+                    Q: Is Stable only available in Calgary?
+                  </Text>
+                  <Text style={styles.body}>
                     A: Stable can be used across North America but most support
                     is currently located in Calgary. As of December 2018 Stable
                     has plans to expand its support to Saskatchewan, Edmonton
@@ -86,10 +90,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: Is there a desktop version of Stable?</Text>
-                  <Text>
+                  <Text style={styles.body}>
+                    Q: Is there a desktop version of Stable?
+                  </Text>
+                  <Text style={styles.body}>
                     A: No, at the moment Stable is only availble through the app
                     on Apple or Android devices.
                   </Text>
@@ -97,10 +103,10 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: How do I park with Stable?</Text>
-                  <Text>
+                  <Text style={styles.body}>Q: How do I park with Stable?</Text>
+                  <Text style={styles.body}>
                     A: Stable is a real time App so treat it just like a normal
                     meter spot. We currently cannot reserve spots, using the map
                     view locate a sport with a green pin, make sure the spot can
@@ -111,10 +117,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: Why are there no spots where I want to park?</Text>
-                  <Text>
+                  <Text style={styles.body}>
+                    Q: Why are there no spots where I want to park?
+                  </Text>
+                  <Text style={styles.body}>
                     A: Stable relies on people in the community to provide
                     access to there private parking spots. So please tell your
                     friends and community members about our App.
@@ -123,10 +131,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: Can I reserve a parking spot?</Text>
-                  <Text>
+                  <Text style={styles.body}>
+                    Q: Can I reserve a parking spot?
+                  </Text>
+                  <Text style={styles.body}>
                     A: No this feature is currently unavailable, but with Peter
                     Parker having such low prices you can book a spot and start
                     paying before you arrive.
@@ -135,12 +145,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>
+                  <Text style={styles.body}>
                     Q: Is there a minimum amount of time to book a parking spot?
                   </Text>
-                  <Text>
+                  <Text style={styles.body}>
                     A: There is no minimum time to park however, there is a
                     minimum 50¢ charge.
                   </Text>
@@ -148,10 +158,12 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text>Q: Can I report bad behaviour?</Text>
-                  <Text onPress={this.handleEmail}>
+                  <Text style={styles.body}>
+                    Q: Can I report bad behaviour?
+                  </Text>
+                  <Text style={styles.body} onPress={this.handleEmail}>
                     A: Yes you can report any user by emailing our support team
                   </Text>
                   <Text onPress={this.handleEmail} style={styles.email}>
@@ -161,17 +173,20 @@ export default class Help extends React.Component {
               </CardItem>
             </Card>
             <Card>
-              <CardItem>
+              <CardItem style={styles.card}>
                 <Body>
-                  <Text onPress={() => this.props.navigation.navigate("Home")}>
+                  <Text
+                    style={styles.body}
+                    onPress={() => this.props.navigation.navigate("Home")}
+                  >
                     Back to Home
                   </Text>
                 </Body>
               </CardItem>
             </Card>
           </Content>
-        </Container>
-      </ScrollView>
+        </ScrollView>
+      </View>
     );
   }
 }
@@ -180,31 +195,47 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     textAlign: "center",
-    padding: 4
+    padding: 4,
+    color: "white"
   },
   header: {
-    height: 75
+    height: 75,
+    backgroundColor: "#3c3c3c",
+    fontFamily: "sans-serif-thin"
   },
   button: {
     position: "absolute",
     left: 0,
     marginTop: 20,
     paddingBottom: 20,
-    paddingTop: 0
+    paddingTop: 0,
+    backgroundColor: "#3c3c3c"
   },
   parker: {
     color: "white",
-    fontSize: 30,
-    fontStyle: "italic",
-    marginTop: 20
+    fontSize: 25,
+    letterSpacing: 1,
+    marginTop: 20,
+    fontFamily: "sans-serif-thin"
   },
   icon: {
     marginTop: 20
   },
-  scrollview: {
-    flex: 1
-  },
   email: {
     color: "#3366BB"
+  },
+  card: {
+    backgroundColor: "#8a8a8a"
+  },
+  body: {
+    color: "#FFFFFF",
+    fontFamily: "sans-serif-thin"
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#2f2f2f"
+  },
+  scrollview: {
+    flex: 1
   }
 });
